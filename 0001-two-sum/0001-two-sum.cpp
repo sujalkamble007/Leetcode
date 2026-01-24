de7ac2fdc6e8272f624797ad75944d43b7a98386
SUJAL KAMBLE <119871977+sujalkamble007@users.noreply.github.com>
2026-01-24 06:12:48 +0530

    Time: 3 ms (67.85%), Space: 14.9 MB (19.4%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //better solution 
-        // we create a hashmapp looling for the solution a+b=target 
-        //algorithms
-            //1.create the map<int,int>
-        int n=nums.size();
-        map<int,int>mapp;    
-            //2.iterate thorught array 
-        for(int i=0;i<n;i++){
-                //calculate curr=nums[i] and next =target- curr
-                int curr=nums[i];
-                int next=target-curr;
-                
-                //check the condition
-                if(mapp.find(next)!= mapp.end()){
-                    //if present return true or reture {mpp[next],i}
-                    return {mapp[next],i};
-                }
-                // push it into mapp
-                mapp[curr]=i;
-        }     
-         return {-1,-1};           
-                    //else return false or return {-1,-1}
-    
+        int n = nums.size();
+        unordered_map<int , int >mapp;
+        for(int i=0 ;i<n;i++){
+            int curr = nums[i];
+            int next = target - curr;
+            if(mapp.find(next) != mapp.end())
+                return {mapp[next] , i};
+
+            mapp[curr] = i;
+        }
+        return {};
     }
 };
