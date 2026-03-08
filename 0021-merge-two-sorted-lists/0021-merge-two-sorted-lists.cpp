a9c5a2f2c9c3899007a5552671707beeca30c2c4
SUJAL KAMBLE <119871977+sujalkamble007@users.noreply.github.com>
2026-03-08 20:03:43 +0530

    Time: 0 ms (100%), Space: 19.7 MB (8.6%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,40 +10,25 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode*   head1, ListNode* head2) {
-        //using the concept of dummy node
-        //keeping two pointer t1=list1 and t2 =list2
-        //iterate till one might finish off
-            //check which is smaller 
-                //1. temp->next = t1  || t2
-                //2. t1=t1->next || t2=t2->next
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* t1 = list1;
+        ListNode* t2 = list2;
 
-                //3.. temp=temp->next
-        //if(t1 != NULL ) temp =t1;
-            //else temp=t2;
-
-        //return dummy->next;
-        ListNode* t1=head1;
-        ListNode* t2=head2;
-
-        ListNode* dummy =new ListNode(-1);
-        ListNode* temp=dummy;
-
-        while(t1!=NULL && t2!=NULL){
-            if(t1->val >= t2->val){
-                temp->next=t2;
-                t2=t2->next;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        while(t1 != NULL && t2 != NULL){
+            if(t1->val > t2->val){
+                temp->next = new ListNode(t2->val);
+                t2 = t2->next;
             }else{
-                temp->next=t1;
-                t1=t1->next;
+                temp->next = new ListNode(t1->val);
+                t1 = t1->next;
             }
-            temp=temp->next;
+            temp = temp->next;
         }
-
-        if(t1 != NULL) temp->next=t1;
-        else temp->next=t2;
+        if(t1 != NULL)temp->next = t1;
+        else temp->next = t2;
 
         return dummy->next;
-
     }
 };
