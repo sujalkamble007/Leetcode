3daeada6c8b32d8edd992e5111fff496983d2290
SUJAL KAMBLE <119871977+sujalkamble007@users.noreply.github.com>
2026-03-09 15:06:43 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.37%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    double Pow(double x,int n){
-        //base case
-        if(n==0)return 1;
-        double temp=Pow(x,n/2);
-        temp=temp*temp;
+    double solve(double x , int n){
+        if(n == 0) return 1 ;
+        double temp = solve(x , n/2);
+        temp = temp* temp ;
 
-        if(n%2==0 )return temp;
-        else return x*temp;
-
+        if(n%2 == 0 )return temp;
+        return x*temp;
     }
     double myPow(double x, int n) {
-        if(n>0)return Pow(x,n);
-        else return 1/Pow(x,n);
+        if(n < 0) return 1 / solve( x , n);
+        return solve(x , n);
     }
 };
