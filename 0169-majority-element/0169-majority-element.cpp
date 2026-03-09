992a885a48844e7efd36bd5aa290f9dbd6dfbb98
SUJAL KAMBLE <119871977+sujalkamble007@users.noreply.github.com>
2026-03-09 15:11:12 +0530

    Time: 1 ms (36.29%), Space: 28.3 MB (7.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //moore voting Algorithmmm
-        //States that count ==0 assign majority
-       
-      
-     int n=nums.size();
-     int count=0;
-    int  majority=0;
-
-    for(int i=0;i<n;i++){
-        if(count== 0) majority=nums[i];
- //count++ if nums[i]==mojority
-        if(nums[i]==majority) count++;  //count-- if nums[i]!=majority
-        else count--;
-
-    }   
-    return majority;
+        int n= nums.size();
+        unordered_map<int , int>mapp;
+        for(auto &i : nums){
+            mapp[i]++;
+            if(mapp[i] > n/2)return i;
+        }
+        return -1;
     }
 };
