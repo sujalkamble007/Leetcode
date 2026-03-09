60ebbada579161309fe3cd54a33321af1e366db6
SUJAL KAMBLE <119871977+sujalkamble007@users.noreply.github.com>
2026-03-09 15:45:26 +0530

    Time: 3 ms (53.45%), Space: 26.4 MB (27.75%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,44 +1,22 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        //moore Voting Algorithm finding majority (ele1 ,ele2)
-        int n=nums.size();
-        int cnt1=0;
-        int cnt2=0;
-        int ele1=INT_MIN;
-        int ele2=INT_MIN;
-
-        for(int i=0;i<n;i++){
-            if(cnt1==0 && nums[i]!=ele2){
-                cnt1=1;
-                ele1=nums[i];
-            }
-            else if(cnt2==0 && nums[i]!=ele1){
-                cnt2=1;
-                ele2=nums[i];
-            }
-            else if(nums[i]==ele1) cnt1++;
-            else if(nums[i]==ele2) cnt2++;
-            else{
-                cnt1--;
-                cnt2--;
-            }
+        unordered_map<int , int >mapp;
+        int n = nums.size();
+        int target = n/3;
+        vector<int> ans;
+        for(auto i : nums){
+            mapp[i]++;
         }
 
-        //counting occurences
-        int count1=0;
-        int count2=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==ele1)count1++;
-            if(nums[i]==ele2)count2++;
+        for(auto i : mapp){
+            if(ans.size() == 0 || ans[0] != i.first){
+                if(i.second > target){
+                    ans.push_back(i.first);
+                }
+            }
+            if(ans.size() == 2 )break;
         }
-        //verify condition mini=(n/3)
-        vector<int>ans;
-        int mini = int(n / 3) + 1;
-        if(count1 >= mini)ans.push_back(ele1);
-        if(count2 >= mini)ans.push_back(ele2);
-
-        //return result
         return ans;
     }
 };
