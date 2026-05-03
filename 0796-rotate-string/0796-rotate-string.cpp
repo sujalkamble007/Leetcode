b972976395db35f1d2abe69d6a2e41f7a384b024
SUJAL KAMBLE <119871977+sujalkamble007@users.noreply.github.com>
2026-05-03 11:18:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.size() != goal.size()) return false;
-        s +=s;
-        for(int i=0;i<s.size() - goal.size() ;i++){
-            if(s.substr(i , goal.size()) == goal)return true;
-        }
-        return false;
+        if(s.size() != goal.size())return false;
+        return (s+s).find(goal) != string::npos;
     }
 };
