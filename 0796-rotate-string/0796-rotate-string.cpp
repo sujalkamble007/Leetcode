f6d32a22f5397889065b587df1ab1c69e050dbe4
SUJAL KAMBLE <119871977+sujalkamble007@users.noreply.github.com>
2026-05-03 11:15:08 +0530

    Time: 0 ms (100%), Space: 8.8 MB (15.19%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size() != goal.size()) return false;
+        s +=s;
+        for(int i=0;i<s.size();i++){
+            if(s.substr(i , goal.size()) == goal)return true;
+        }
+        return false;
+    }
+};
