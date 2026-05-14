66c2dd80baeeb5b14e7820bf9e9278e1f1935867
SUJAL KAMBLE <119871977+sujalkamble007@users.noreply.github.com>
2026-05-14 20:36:20 +0530

    Time: 3 ms (20.41%), Space: 26.4 MB (73.39%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int n = nums.back();
+
+        if(nums.size() != n + 1) return false;
+
+        for(int i = 0; i < n - 1; i++) {
+            if(nums[i] != i + 1) return false;
+        }
+
+        return nums[n - 1] == n && nums[n] == n;
+    }
+};
