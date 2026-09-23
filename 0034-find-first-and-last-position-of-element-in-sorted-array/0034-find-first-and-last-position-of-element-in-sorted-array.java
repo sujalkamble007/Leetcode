2d4d5a3b58af0c1a104dd915184bac37845b4513
SUJAL KAMBLE <119871977+sujalkamble007@users.noreply.github.com>
2026-09-23 15:20:17 +0530

    Time: 0 ms (100%), Space: 48.3 MB (31.8%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,48 +1,46 @@
 class Solution {
 
-    public int lowerBound(int[] arr, int target) {
-
+    public int lowerBound(int[] arr , int target){
         int low = 0;
         int high = arr.length - 1;
-        int ans = arr.length;
 
-        while (low <= high) {
+        int ans = arr.length ;
+
+        while(low <= high){
             int mid = (low + high) / 2;
-            if (arr[mid] >= target) {
+            if(arr[mid] >= target){
                 ans = mid;
                 high = mid - 1;
-            } else {
+            }else {
                 low = mid + 1;
             }
-
         }
         return ans;
     }
 
-    public int upperBound(int[] arr, int target) {
+    public int upperBound(int[] arr , int target){
         int low = 0;
         int high = arr.length - 1;
-        int ans = arr.length;
+        int ans = arr.length ;
 
-        while (low <= high) {
+        while(low <= high){
             int mid = (low + high) / 2;
-            if (arr[mid] > target) {
-                ans = mid;
-                high = mid - 1;
-            } else
+
+            if(arr[mid] > target){
+                ans = mid ;
+                high = mid -1 ;
+            }else {
                 low = mid + 1;
+            }
         }
         return ans;
     }
 
     public int[] searchRange(int[] nums, int target) {
-        int lb = lowerBound(nums, target);
+        int lb = lowerBound(nums , target);
+        if(lb == nums.length  || nums[lb] != target) return new int[]{-1 , -1};
+        int ub = upperBound(nums , target);
 
-        //COndition nums[lb] != target or lb == nums.length return invalid
-        if (lb == nums.length || nums[lb] != target)
-            return new int[] { -1, -1 };
-        int ub = upperBound(nums, target);
-
-        return new int[] { lb, ub - 1 };
+        return new int[]{lb , ub - 1};
     }
 }
